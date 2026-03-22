58bed124a5e0eaf42d746599f3eb86d0f7bb8e0a
wahoovisuals <dasavagebeast.11@gmail.com>
2026-03-22 19:32:04 +0530

    fix: remove duplicate sidebar/wrapper from UPI page, match Cash Management spacing

--- FRONTEND/galaxia/app/admin3/upi-management/page.tsx
+++ FRONTEND/galaxia/app/admin3/upi-management/page.tsx
@@ -1,13 +1,7 @@
-import AdminSidebar from "../../components/AdminSidebar";
 import UpiManagementClient from "./UpiManagementClient";
 
 export default function UpiManagementPage() {
     return (
-        <div className="flex min-h-screen bg-[#f8f9fc]" style={{ fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif" }}>
-            <AdminSidebar isAdmin3 />
-            <main className="flex-1 p-6 lg:p-8 lg:ml-72 mt-12 lg:mt-0">
-                <UpiManagementClient />
-            </main>
-        </div>
+        <UpiManagementClient />
     );
 }
