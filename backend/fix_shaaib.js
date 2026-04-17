97dd4837de753484aeef0309e349226413c20c3f
wahoovisuals <dasavagebeast.11@gmail.com>
2026-04-17 23:30:31 +0530

    chore: remove temp debug scripts

--- backend/fix_shaaib.js
+++
@@ -1,7 +0,0 @@
-const{PrismaClient}=require('@prisma/client');
-const p=new PrismaClient();
-(async()=>{
-  const r=await p.ddBooking.updateMany({where:{bookingRef:'DD-20260417-027'},data:{totalAmount:2100}});
-  console.log('Updated shaaib totalAmount to 2100:',r);
-  await p.$disconnect();
-})();
