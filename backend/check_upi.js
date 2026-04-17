51f7c989d05423741ee0a3a57a7dd98c276487ed
wahoovisuals <dasavagebeast.11@gmail.com>
2026-04-17 22:48:55 +0530

    chore: remove temp debug script

--- backend/check_upi.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-(async () => {
-    const admins = await p.adminAccount.findMany({
-        select: { id: true, username: true, role: true, displayName: true }
-    });
-    console.log("ADMINS:", JSON.stringify(admins, null, 2));
-    await p.$disconnect();
-})();
