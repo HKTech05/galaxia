026476bb7111440e988c39a1c2016fde52ceb49a
wahoovisuals <dasavagebeast.11@gmail.com>
2026-04-27 14:34:07 +0530

    chore: remove test scripts

--- backend/get-admin.js
+++
@@ -1,6 +0,0 @@
-const{PrismaClient}=require("@prisma/client");
-const p=new PrismaClient();
-p.adminAccount.findFirst({where:{role:"owner"},select:{username:true,plainPassword:true}}).then(a=>{
-  console.log(JSON.stringify(a));
-  p.$disconnect();
-});
