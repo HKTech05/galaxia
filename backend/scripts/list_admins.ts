45f5e52c2050b6338a65fe0954076277a27c000c
wahoovisuals <dasavagebeast.11@gmail.com>
2026-07-25 12:04:34 +0530

    chore: remove temp script

--- backend/scripts/list_admins.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-    const accs = await prisma.adminAccount.findMany();
-    console.log(JSON.stringify(accs, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
